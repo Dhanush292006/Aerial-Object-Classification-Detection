b29024e2a73154d413e156597e9dd5e8143fbf54
Dhanush292006 <dhanushs99777@gmail.com>
2026-04-16 19:01:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,18 @@
 import streamlit as st
 from PIL import Image
+from ultralytics import YOLO
+
+# Load YOLO model
+model = YOLO("best.pt")
 
 st.title("Aerial Object Detection (Bird vs Drone)")
 
 uploaded_file = st.file_uploader("Upload Image", type=["jpg","png","jpeg"])
 
-if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image")
+if uploaded_file:
+    img = Image.open(uploaded_file)
+    st.image(img, caption="Uploaded Image")
 
-    try:
-        from ultralytics import YOLO
+    results = model(img)
 
-        # Load model ONLY after upload (prevents crash)
-        model = YOLO("yolov8n.pt")
-
-        results = model(image)
-
-        st.image(results[0].plot(), caption="Detection Result")
-
-    except Exception as e:
-        st.error(f"Error: {e}")
+    st.image(results[0].plot(), caption="Detection Result")
