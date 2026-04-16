f4c87936e37d9d50cac4d62ed0a7ed5e9f7120b6
Dhanush292006 <dhanushs99777@gmail.com>
2026-04-16 18:43:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from PIL import Image
 
-st.title("Aerial Object Classification")
+st.title("Aerial Object Classification (Bird vs Drone)")
 
 uploaded_file = st.file_uploader("Upload Image", type=["jpg","png","jpeg"])
 
@@ -9,5 +9,5 @@ if uploaded_file:
     img = Image.open(uploaded_file)
     st.image(img, caption="Uploaded Image")
 
-    # Demo output (since TF removed)
-    st.success("Prediction working (UI Demo Mode)")
+    # Demo output
+    st.success("Prediction working (Demo Mode)")
